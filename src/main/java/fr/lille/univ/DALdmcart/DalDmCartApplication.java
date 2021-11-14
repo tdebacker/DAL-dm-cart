9fd58aaa36493ebe2e0fd3641f558a8c2c6459e1
debacker <tanguy.debacker.etu@univ-lille.fr>
2021-11-14 15:57:09 +0100

    init project

---
+++ src/main/java/fr/lille/univ/DALdmcart/DalDmCartApplication.java
@@ -0,0 +1,13 @@
+package fr.lille.univ.DALdmcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalDmCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalDmCartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/lille/univ/DALdmcart/DalDmCartApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.lille.univ.DALdmcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalDmCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
